a5dc4f421e5c41b61f645f23c427415d416b26ff
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-11-14 18:22:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Fakecheater/MUSIC-NEW",
+    "https://github.com/Fakecheater77x/MUSIC-NEW",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
